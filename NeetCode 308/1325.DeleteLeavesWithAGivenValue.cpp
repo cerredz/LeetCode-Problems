6b64ecd614fb3296401b25f44f9c9154253f383e
cerredz <110927971+cerredz@users.noreply.github.com>
2023-12-14 23:59:01 -0500

    Answer question 1325 on leetcode

---
+++ NeetCode 308/1325.DeleteLeavesWithAGivenValue.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+
+    void deleteLeaves(TreeNode*& root, int target) {
+
+        if(!root) {
+            return;
+        }
+
+        // traverse through tree
+        deleteLeaves(root->left, target);
+        deleteLeaves(root->right, target);
+        
+        if(root->val == target && !root->left && !root->right) {
+            // recursion will continously pop off all leaves AND connecting leaf nodes = target
+            root = NULL;
+        }
+        return;
+    }
+
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if(!root) return root;
+        deleteLeaves(root, target);
+        return root;
+    }
+};
